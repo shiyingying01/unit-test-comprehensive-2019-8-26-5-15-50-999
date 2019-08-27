d5b5e48349c8e6699ac451b167347b98e70cd8d6
shiyingying01 <718275303@qq.com>
2019-08-27 14:52:39 +0800

    test

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -1,18 +1,29 @@
 package tw;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import tw.commands.GuessInputCommand;
 
-/**
- * Created by jxzhong on 2017/5/16.
- */
 public class Main {
+	private static final int RUN_TIMES = 6;
 
-    private static final int RUN_TIMES = 6;
-
-    public static void main(String[] args) throws Exception {
-        for (int i = 0; i < RUN_TIMES; i++) {
-            String input = new GuessInputCommand().input();
-            System.out.println(input);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		GuessInputCommand guessInputCommand = new GuessInputCommand();
+		List<String> answer = guessInputCommand.getAnswer();
+		answer.forEach(p -> System.out.print(p + " "));
+		for (int i = 0; i < RUN_TIMES; i++) {
+			String input = guessInputCommand.input();
+			List<String> inputList = Arrays.asList(input.split(" "));
+			List<String> numA = guessInputCommand.getANum(inputList, answer);
+			List<String> numB = guessInputCommand.getBNum(inputList, answer, numA);
+			String result = guessInputCommand.printResult(inputList, numA, numB);
+			System.out.println(result);
+			if (result.equals("4A0B 胜利，一切正确")) {
+				break;
+			}
+		}
+		System.out.println("游戏结束！");
+	}
 }
--- src/main/java/tw/commands/GuessInputCommand.java
+++ src/main/java/tw/commands/GuessInputCommand.java
@@ -3,20 +3,83 @@ package tw.commands;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
-/**
- * Created by jxzhong on 2017/5/19.
- */
 public class GuessInputCommand {
+	private BufferedReader bufferedReader;
 
-    private BufferedReader bufferedReader;
+	public GuessInputCommand() {
+		bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	}
 
-    public GuessInputCommand() {
-        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    }
+	public String input() throws IOException {
+		System.out.println("------Please input your answer as x x x x , x <10 ------");
+		return bufferedReader.readLine();
+	}
 
-    public String input() throws IOException {
-        System.out.println("------Please input your answer as x x x x , x <10 ------");
-        return bufferedReader.readLine();
-    }
+	public List<String> getAnswer() {
+		Set<String> list1 = new HashSet<>();
+		while (list1.size() < 4) {
+			list1.add(String.valueOf(new Random().nextInt(9)));
+		}
+		return new ArrayList<String>(list1);
+	}
+
+	public List<String> getANum(List<String> inputList, List<String> answer) {
+		return inputList.stream()
+				.filter(p -> answer.contains(p)&&inputList.indexOf(p)==answer.indexOf(p))
+				.collect(Collectors.toList());
+	}
+	
+	public List<String> getBNum(List<String> inputList, List<String> answer,List<String> numA) {
+		return inputList.stream()
+				.filter(p -> answer.contains(p)&&(!numA.contains(p)))
+				.collect(Collectors.toList());
+	}
+
+	public String printResult(List<String> inputList,List<String> numA, List<String> numB) {
+		if(inputList.stream().distinct().count()<4) {
+			return "输入错误，再次输入";
+		}
+		StringBuilder result =new StringBuilder();
+		int numASize=numA.size();
+		int numBSize=numB.size();
+		if(numASize==4) {
+			return numASize+"A"+numBSize+"B"+" 胜利，一切正确";
+		}
+		if(numASize==0) {
+			if(numBSize==0) {
+				return numASize+"A"+numBSize+"B"+" 都错了";
+			}else if(numBSize==4) {
+				return numASize+"A"+numBSize+"B"+" 4个数字位置错误";
+			}
+		}
+		result.append(numASize+"A"+numBSize+"B ");
+		if(numASize>0) {	
+			
+			numA.forEach(p -> result.append(p+"和"));
+			result.deleteCharAt(result.length()-1);
+			result.append("正确");
+			if(numBSize>0) {	
+				result.append("，");
+			}
+		}
+		if(numBSize>0) {
+			Collections.sort(numB);
+			numB.forEach(p -> result.append(p+"和"));
+			result.deleteCharAt(result.length()-1);
+			result.append("位置错误");
+		}
+		
+		return result.toString();
+	}
 }
